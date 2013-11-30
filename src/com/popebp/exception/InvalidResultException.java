7d454bc0d579f96cbf887f9e244f76d7e898c97e
sparticvs <sparticvs@popebp.com>
2013-11-30 10:48:29 -0500

    Add InvalidResultException
    Sometime in the future these libraries will be moved around into a
    better layout. Some other time, not right now.

---
+++ src/com/popebp/exception/InvalidResultException.java
@@ -0,0 +1,30 @@
+package com.popebp.exception;
+
+public class InvalidResultException extends Exception {
+
+	public InvalidResultException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidResultException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidResultException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidResultException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidResultException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
