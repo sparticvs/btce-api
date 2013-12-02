2d1fc9ee98edb9261f10b5c69f5834793cf9a730
sparticvs <sparticvs@popebp.com>
2013-12-02 08:17:28 -0500

    Added Uri Building to some Methods

--- src/com/popebp/bitcoin/exchange/btce/BTCe.java
+++ src/com/popebp/bitcoin/exchange/btce/BTCe.java
@@ -2,6 +2,8 @@ package com.popebp.bitcoin.exchange.btce;
 
 import org.json.JSONObject;
 
+import android.net.Uri;
+
 public class BTCe {
 	public static final String API_URL = "https://btc-e.com/tapi";
 	private String apiKey = null;
@@ -27,26 +29,42 @@ public class BTCe {
 	}
 	
 	public Result getInfo() {
+		String uri = BTCe.buildTradeUrl("getInfo");
 		return new GetInfoResult();
 	}
 	
 	public Result getTransactionHistory(Parameters params) {
+		String uri = BTCe.buildTradeUrl("TransHistory");
 		return new TransactionHistoryResult();
 	}
 	
 	public Result getTradeHistory(Parameters params) {
+		String uri = BTCe.buildTradeUrl("TradeHistory");
 		return new TradeHistoryResult();
 	}
 	
 	public Result getActiveOrders(Parameters params) {
+		String uri = BTCe.buildTradeUrl("ActiveOrders");
 		return new ActiveOrdersResult();
 	}
 	
 	public Result doTrade(Parameters params) {
+		String uri = BTCe.buildTradeUrl("Trade");
 		return new TradeResult();
 	}
 	
 	public Result doCancelOrder(Parameters params) {
+		String uri = BTCe.buildTradeUrl("CancelOrder");
 		return new CancelOrderResult();
+	}
+	
+	private static String buildTradeUrl(String action) {
+		
+		Uri uri = Uri.parse(API_URL);
+		
+		Uri.Builder builder = uri.buildUpon();
+		builder.appendPath(action);
+		
+		return builder.toString();
 	}
 }
